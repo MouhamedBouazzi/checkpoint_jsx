a1e46bd1271b4f9b0624fb374b9d38f1d9193741
unknown <mouhamed.bouazzi@gmail.com>
2022-01-05 20:45:04 +0000

    I need Help

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import imageInSrc from "./assests/imageInSrc.png"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <div>
+    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+      <h1 className="title red">Mouhamed</h1>
+      <br />
+      <img src={imageInSrc}   alt="hohoho"/>
+      <br />
+      <img src="/imageInPublic.png" alt="hoihoi" />
     </div>
+    <div>
+      
+      {/* <img src="/imageInPublic.png"  alt="Meme21"/> */}
+      {/* <img src={imageInSrc}  alt="meme35"/> */}
+    </div>
+    
+    <video width={320} height={240} controls>
+      <source src="myVideo.mp4" type="video/mp4" />
+    </video>
+  </div>
+  </>
   );
 }
 
